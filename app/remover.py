62395ccc2982f7eea273c7936eaae7f087d4a0ea
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-12 12:11:42 +0300

    remover.py

--- app/remover.py
+++ app/remover.py
@@ -1,10 +1,11 @@
-# app/remover.py
-
-from rembg import remove
+from rembg import new_session, remove
 from PIL import Image
 import io
 
-def remove_background(image_bytes: bytes) -> Image.Image:
+# Crea una sessione con il modello migliore
+session = new_session("isnet-general-use")
+
+def remove_background(image_bytes: bytes):
     input_image = Image.open(io.BytesIO(image_bytes)).convert("RGBA")
-    output_image = remove(input_image)
+    output_image = remove(input_image, session=session)
     return output_image
