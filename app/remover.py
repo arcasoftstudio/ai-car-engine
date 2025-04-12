d8f066a04064e69ba2e15ea1f8a1002129dc9919
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-12 15:26:43 +0300

    remover.py

--- app/remover.py
+++ app/remover.py
@@ -1,15 +1,17 @@
 from carvekit.api.high import Interface
-from carvekit.ml.wrap.modnet import IS_MODNET
+from carvekit.ml.init import init_interface
 from PIL import Image
 import io
-import numpy as np
 
-# Setup del modello CarveKit con MODNet
-interface = Interface(
-    seg_pipe=IS_MODNET(),
-    pre_pipe=None,
-    post_pipe=None,
-    device="cuda",  # usa la GPU!
+# Inizializza interfaccia MODNet
+interface = init_interface(
+    model_type="modnet",
+    device="cuda",  # usa la tua GPU
+    seg_mask_size=640,
+    refine_mode="full",
+    trimap_prob_threshold=231,
+    trimap_dilation=30,
+    trimap_erosion_iters=5
 )
 
 def remove_background(image_bytes: bytes):
