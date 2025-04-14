55164689a8a15b2bfbc267af4e89e9dc5e52f650
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-14 22:20:48 +0300

    main.py

--- app/main.py
+++ app/main.py
@@ -15,3 +15,15 @@ async def remove_bg(file: UploadFile = File(...)):
     result_image.save(img_io, format='PNG')
     img_io.seek(0)
     return StreamingResponse(img_io, media_type="image/png")
+
+from app.sam_remover import remove_background_sam  # importa la funzione nuova
+
+
+@app.post("/remove-background-sam")
+async def remove_bg_sam(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    result_image = remove_background_sam(image_bytes)
+    img_io = io.BytesIO()
+    result_image.save(img_io, format="PNG")
+    img_io.seek(0)
+    return StreamingResponse(img_io, media_type="image/png")
