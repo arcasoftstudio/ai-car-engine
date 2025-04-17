dd52f6af63f48a883ae34be46e433a7f85c46839
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-17 16:13:23 +0300

    modnet_remover.py

--- app/modnet_remover.py
+++ app/modnet_remover.py
@@ -25,11 +25,12 @@ def download_model():
 download_model()
 
 def load_modnet():
-    model = MODNet(backbone_pretrained=False)
+    model = MODNet()
     state_dict = torch.load(MODEL_PATH, map_location='cpu')
     model.load_state_dict(state_dict)
     model.eval()
     return model
+
 
 def remove_background_modnet(image_bytes: bytes):
     input_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
@@ -41,7 +42,7 @@ def remove_background_modnet(image_bytes: bytes):
     modnet = load_modnet()
 
     with torch.no_grad():
-        _, _, matte = modnet(im_tensor, inference=True)
+        matte = modnet(im_tensor)
         matte = interpolate(matte.unsqueeze(0), size=(h, w), mode='bilinear').squeeze().numpy()
 
     fg = im.astype(np.float32) / 255
